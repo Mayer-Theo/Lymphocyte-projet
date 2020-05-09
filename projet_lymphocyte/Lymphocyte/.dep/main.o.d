ae71e65a0d76d27426773cadfaee8eca4b7f2c7c
Theo <theo.mayer@epfl.ch>
2020-05-09 10:18:27 +0200

    test

--- projet_lymphocyte/Lymphocyte/.dep/main.o.d
+++ projet_lymphocyte/Lymphocyte/.dep/main.o.d
@@ -132,7 +132,7 @@ build/obj/main.o: main.c \
  ../../lib/e-puck2_main-processor/ChibiOS//os/hal/include/mmc_spi.h \
  ../../lib/e-puck2_main-processor/ChibiOS//os/hal/include/serial_usb.h \
  ../../lib/e-puck2_main-processor/src/memory_protection.h \
- ../../lib/e-puck2_main-processor/src/usbcfg.h main.h \
+ ../../lib/e-puck2_main-processor/src/sensors/proximity.h main.h \
  ../../lib/e-puck2_main-processor/src/camera/dcmi_camera.h \
  ../../lib/e-puck2_main-processor/ChibiOS_ext/os/hal/include/dcmi.h \
  ../../lib/e-puck2_main-processor/ChibiOS_ext/os/hal/include/../ports/STM32/STM32F4xx/dcmi_lld.h \
@@ -142,11 +142,7 @@ build/obj/main.o: main.c \
  ../../lib/e-puck2_main-processor/src/msgbus/messagebus.h \
  d:\downloads\eclipse_e-puck2_win32_29_jan_2020\eclipse_e-puck2\tools\gcc-arm-none-eabi-7-2017-q4-major-win32\arm-none-eabi\include\unistd.h \
  d:\downloads\eclipse_e-puck2_win32_29_jan_2020\eclipse_e-puck2\tools\gcc-arm-none-eabi-7-2017-q4-major-win32\arm-none-eabi\include\sys\unistd.h \
- ../../lib/e-puck2_main-processor/src/parameter/parameter.h \
- ../../lib/e-puck2_main-processor/src/motors.h \
- ../../lib/e-puck2_main-processor/src/camera/po8030.h \
- ../../lib/e-puck2_main-processor/ChibiOS//os/hal/lib/streams/chprintf.h \
- pi_regulator.h process_image.h
+ ../../lib/e-puck2_main-processor/src/parameter/parameter.h
 
 d:\downloads\eclipse_e-puck2_win32_29_jan_2020\eclipse_e-puck2\tools\gcc-arm-none-eabi-7-2017-q4-major-win32\arm-none-eabi\include\stdio.h:
 
@@ -416,7 +412,7 @@ mcuconf.h:
 
 ../../lib/e-puck2_main-processor/src/memory_protection.h:
 
-../../lib/e-puck2_main-processor/src/usbcfg.h:
+../../lib/e-puck2_main-processor/src/sensors/proximity.h:
 
 main.h:
 
@@ -439,13 +435,3 @@ d:\downloads\eclipse_e-puck2_win32_29_jan_2020\eclipse_e-puck2\tools\gcc-arm-non
 d:\downloads\eclipse_e-puck2_win32_29_jan_2020\eclipse_e-puck2\tools\gcc-arm-none-eabi-7-2017-q4-major-win32\arm-none-eabi\include\sys\unistd.h:
 
 ../../lib/e-puck2_main-processor/src/parameter/parameter.h:
-
-../../lib/e-puck2_main-processor/src/motors.h:
-
-../../lib/e-puck2_main-processor/src/camera/po8030.h:
-
-../../lib/e-puck2_main-processor/ChibiOS//os/hal/lib/streams/chprintf.h:
-
-pi_regulator.h:
-
-process_image.h:
--- projet_lymphocyte/Lymphocyte/main.c
+++ projet_lymphocyte/Lymphocyte/main.c
@@ -6,33 +6,9 @@
 #include "ch.h"
 #include "hal.h"
 #include "memory_protection.h"
-#include <usbcfg.h>
+#include "sensors/proximity.h"
 #include <main.h>
-#include <motors.h>
-#include <camera/po8030.h>
-#include <chprintf.h>
 
-#include <pi_regulator.h>
-#include <process_image.h>
-
-void SendUint8ToComputer(uint8_t* data, uint16_t size) 
-{
-	chSequentialStreamWrite((BaseSequentialStream *)&SD3, (uint8_t*)"START", 5);
-	chSequentialStreamWrite((BaseSequentialStream *)&SD3, (uint8_t*)&size, sizeof(uint16_t));
-	chSequentialStreamWrite((BaseSequentialStream *)&SD3, (uint8_t*)data, size);
-}
-
-static void serial_start(void)
-{
-	static SerialConfig ser_cfg = {
-	    115200,
-	    0,
-	    0,
-	    0,
-	};
-
-	sdStart(&SD3, &ser_cfg); // UART3.
-}
 
 int main(void)
 {
@@ -40,20 +16,8 @@ int main(void)
     halInit();
     chSysInit();
     mpu_init();
+    proximity_start();
 
-    //starts the serial communication
-    serial_start();
-    //start the USB communication
-    usb_start();
-    //starts the camera
-    dcmi_start();
-	po8030_start();
-	//inits the motors
-	motors_init();
-
-	//stars the threads for the pi regulator and the processing of the image
-	pi_regulator_start();
-	process_image_start();
 
     /* Infinite loop. */
     while (1) {
